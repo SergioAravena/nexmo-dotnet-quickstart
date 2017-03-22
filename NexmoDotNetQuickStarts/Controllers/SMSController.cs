862518dc4dca0fd22e869ddfcff37f7f98f93c19
sidsharma27 <sidsharma27@yahoo.com>
2017-03-21 21:21:15 -0700

    Recieve SMS Starter Project

--- NexmoDotNetQuickStarts/Controllers/SMSController.cs
+++ NexmoDotNetQuickStarts/Controllers/SMSController.cs
@@ -36,46 +36,6 @@ namespace NexmoDotNetQuickStarts.Controllers
                 });
                 return View("Index");
             }
-
-            [System.Web.Mvc.HttpGet]
-            public ActionResult Recieve([FromUri]SMS.SMSInbound response)
-            {
-
-                if (null != response.to && null != response.msisdn)
-                {
-                    Debug.WriteLine("-------------------------------------------------------------------------");
-                    Debug.WriteLine("INCOMING TEXT");
-                    Debug.WriteLine("From: " + response.msisdn);
-                    Debug.WriteLine(" Message: " + response.text);
-                    Debug.WriteLine("-------------------------------------------------------------------------");
-                    return new HttpStatusCodeResult(200);
-
-                }
-                else {
-                    Debug.WriteLine("-------------------------------------------------------------------------");
-                    Debug.WriteLine("Endpoint was hit.");
-                    Debug.WriteLine("-------------------------------------------------------------------------");
-                    return new HttpStatusCodeResult(200);
-                
-                }
-
-            }
-
-            [System.Web.Mvc.HttpGet]
-            public ActionResult DLR([FromUri]SMS.SMSDeliveryReceipt response)
-            {
-
-                Debug.WriteLine("-------------------------------------------------------------------------");
-                Debug.WriteLine("DELIVERY RECIEPT");
-                Debug.WriteLine("Message ID: " + response.messageId);
-                Debug.WriteLine("From: " + response.msisdn);
-                Debug.WriteLine("To: " + response.to);
-                Debug.WriteLine("Status: " + response.status);
-                Debug.WriteLine("-------------------------------------------------------------------------");
-
-                return new HttpStatusCodeResult(200);
-            }
-    
     }
 
 }
